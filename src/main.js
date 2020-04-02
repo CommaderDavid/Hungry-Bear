2cb7e0779b26f80de02e4694fbcf347ab3e0c581
David Boedigheimer <stealth259@gmail.com>
2020-04-02 16:58:35 -0700

    Add start of UI code in java.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { nameOfFunction } from './';
+import { HungryBear } from './hungrybear.js';
+import { Levels } from './levels.js';
+
+$(document).ready(function() {
+
+  let time = new Levels();
+  let fuzzy = new HungryBear("Fuzzy");
+
+  $("button#feed").click(function() {
+
+  });
+
+  $("button#sing").click(function() {
+    
+  });
+});
