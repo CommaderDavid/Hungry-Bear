7d2a4648e4da645bdc4e256b374c6080d7d7c0b9
David Boedigheimer <stealth259@gmail.com>
2020-04-07 16:07:02 -0700

    Add 2 variables that run and contain the intervals.

--- src/main.js
+++ src/main.js
@@ -10,13 +10,35 @@ $(document).ready(function() {
   let time = new Levels();
   let fuzzy = new HungryBear("Fuzzy");
 
+  let dayInterval = () => {
+    let day = setInterval(() => {
+      if (time.dayTime > 0) {
+        time.dayTime--;
+        $("#timeOfDay").empty().append(time.dayTime);
+      } else if (time.dayTime === 0) {
+        clearInterval(day);
+        nightInterval();
+        time.nightTime = 20;
+      }
+    }, 4000);
+  };
+
+  let nightInterval = () => {
+    let night = setInterval(() => {
+      if (time.nightTime > 0) {
+        time.nightTime--;
+        $("#timeOfDay").empty().append(time.nightTime);
+      } else if (time.nightTime === 0) {
+        clearInterval(night);
+        dayInterval();
+        time.dayTime = 20;
+      }
+    }, 2000);
+  };
+
   $("button#start").click(function() {
-    time.setDayTime();
-    fuzzy.setHunger();
+    dayInterval();
 
-    if (time.dayTime === 0) {
-
-    }
   });
 
   $("button#feed").click(function() {
