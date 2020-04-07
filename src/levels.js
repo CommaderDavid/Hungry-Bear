87d70e0410b30778dbaf4eab9fb050b86d4aa903
David Boedigheimer <stealth259@gmail.com>
2020-04-07 16:06:28 -0700

    Remove unused and errelivent code.

--- src/levels.js
+++ src/levels.js
@@ -4,35 +4,4 @@ export class Levels {
     this.dayTime = 20;
     this.nightTime = 20;
   }
-
-  setDayTime() {
-    setInterval(() => {
-      this.dayTime--;
-      console.log(this.dayTime, "Day Time");
-    }, 4000);
-  }
-
-  setNightTime() {
-    setInterval(() => {
-      this.nightTime--;
-    }, 2000);
-  }
-
-  switchToNight() {
-    if (this.dayTime === 0) {
-      this.nightTime = 20;
-      return this.nightTime = true;
-    } else {
-      return this.nightTime = false;
-    }
-  }
-
-  switchToDay() {
-    if (this.nightTime === 0) {
-      this.dayTime = 20;
-      return this.dayTime = true;
-    } else {
-      return this.dayTime = false;
-    }
-  }
 }
